7a26712aef4570bce9b33993a12ec6bb70a3a8ff
DheerajGogoi <dheerajgogoi2@gmail.com>
2023-05-22 22:51:04 +0530

    feat: added rate limiter

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const ejs = require('ejs');
+const rateLimiter = require("express-rate-limit");
 const router_routes = require('./routes/route');
 
 require('dotenv').config();
 
 const app = express();
+
+const limiter = rateLimiter({
+    max: 4,
+    windowMs: 1000,
+    message: "Too many request from this IP, please try again in few seconds."
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +25,9 @@ app.listen(process.env.PORT, function () {
     console.log(`Server started at ${process.env.PORT}`);
 });
 
-app.get('/', (req, res) => {
+app.get('/', limiter, (req, res) => {
     res.redirect('/path/index');
 });
 
 // END POINTS
-app.use('/path', router_routes);
+app.use('/path', limiter, router_routes);
